4112ec29ba9c637145504197c8d123d0eae8a5e0
jmd <jmd2028@gmail.com>
2015-01-10 04:44:44 -0800

    ErfEd: update setup.py.

--- ErfEd/setup.py
+++ ErfEd/setup.py
@@ -1,21 +1,29 @@
-# setup.py for neverrun
+from setuptools import setup, find_packages
 
-from distutils.core import setup
 import os
 
+platforms = [os.path.join('../external/platforms', i) for i in os.listdir('../external/platforms')]
 data_files = [('', ["ErfEd.ini"])]
 
 if os.name == 'nt':
     import py2exe
+    data_files.append(('platforms', platforms))
     setup(name="ErfEd",
           version="0.1",
           description="ERF file format editor.",
           author="jmd",
           data_files = data_files,
-          windows = [ {
+          windows = [{
               "script": "ErfEd.py",
-              "icon_resources" : [(1, os.path.join('../icons',"nwn1-toolset.ico"))]}],
-              options={"py2exe":{"includes":["sip"]}})
+              "icon_resources" : [(1, os.path.join('../external/icons',"nwn1-toolset.ico"))]
+          }],
+          zipfile=None,
+          options={"py2exe":{
+              'compressed': 2,
+              'optimize': 2,
+              'includes': ["sip"],
+              'bundle_files': 1}
+          })
 
 elif os.name == 'posix':
     setup(name="ErfEd",
